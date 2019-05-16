ad3d8417068a40adc1bef481398f076722bcb0ab
LoneMarvel <tinost@gmail.com>
2019-05-16 09:18:21 +0200

    An Updated Ver That Displays IP Address & DHCP Server

--- prdhcp.sh
+++ prdhcp.sh
@@ -9,6 +9,9 @@ fi
 devList=$(ip r | grep default | awk '{print $5}')
 for i in $(echo $devList | tr " " "\n")
 do
+  echo 'IP Address Of '$i'Device Is => '$(ip a s | grep $i | tail -1 | awk '{print $2}')
   echo -n 'Network Device => '$i' => '
   cat $(ls -lt /var/lib/NetworkManager/dhclient-*$i*.lease | head -n 1 | awk '{print $9}') | grep -i dhcp-server | head -n 1 | awk '{print $3}' | sed 's/;$//'
 done
+myExtIP=$(dig TXT +short o-o.myaddr.l.google.com @ns1.google.com | awk -F'"' '{print $2}')
+echo 'Public IP Is => '$myExtIP
